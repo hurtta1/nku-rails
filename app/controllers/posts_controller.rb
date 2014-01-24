af6928e6930dd46ccc6665a9abc6d795f47bae68
AlexHurtt <hurtta1@nku.edu>
2014-01-24 03:22:46 +0000

    index, routes update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,15 @@ class PostsController < ApplicationController
   @post.save
   redirect_to @post
  end
- 
+  
+  def show
+  @post = Post.find(params[:id])
+ end
+  
+  def index
+  @posts = Post.all
+ end
+  
 private
   def post_params
     params.require(:post).permit(:title, :text)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ NkuRails::Application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
-
+  post GET    /posts/:id(.:format)      posts#show
+  posts GET    /posts(.:format)          posts#index
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
