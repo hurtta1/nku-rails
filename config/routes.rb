075681eb1ce2a46687c27d4728f8ebd7052da735
AlexHurtt <hurtta1@nku.edu>
2014-01-24 02:48:58 +0000

    post controller update

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,7 @@ NkuRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-  
-  Blog::Application.routes.draw do
- 
-  resources :posts
- 
-  root to: "welcome#index"
-end
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
