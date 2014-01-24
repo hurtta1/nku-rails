47dc7425f6c7d5b49f02c58a1e268e11b0797de7
AlexHurtt <hurtta1@nku.edu>
2014-01-24 03:33:52 +0000

    post controller update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
   
-  def new
-  end
-  
- def create
-  @post = Post.new(post_params)
- 
-  @post.save
-  redirect_to @post
+ def new
+  @post = Post.new
  end
+ 
+ def create
+  @post = Post.new(params[:post].permit(:title, :text))
+ 
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
+  end
   
   def show
   @post = Post.find(params[:id])
