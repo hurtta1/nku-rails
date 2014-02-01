99181df6d354cae481b6e1ef43ad80e46191990c
AlexHurtt <hurtta1@nku.edu>
2014-02-01 01:51:07 +0000

    image url added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravitar_url(student)
+    gravatar_id = Digest::MD5.hexdigest(student.Email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
