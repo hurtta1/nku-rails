72a4f12c10a52940eb783fbad743264dec2d7188
AlexHurtt <hurtta1@nku.edu>
2014-02-01 02:52:27 +0000

    gravitar & user image input enabled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-  def gravitar_url(student)
-    gravatar_id = Digest::MD5.hexdigest(student.Email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
+  def gravitar_url(student)  
+    if student.Gravitar.blank?
+      gravitar_id = Digest::MD5.hexdigest(student.Email.downcase)
+      "http://gravatar.com/avatar/#{gravitar_id}.png?s=48"
+     else
+     gravitar_id = student.Gravitar
+    end
   end
 end
