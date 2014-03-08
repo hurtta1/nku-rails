f877f4f309e6380a1cf6f235613435831cbdfbe5
AlexHurtt <hurtta1@nku.edu>
2014-03-08 04:00:13 +0000

    doesnt auto-login, seating doesnt work, attendance doesnt work

--- app/controllers/bueller_controller.rb
+++ app/controllers/bueller_controller.rb
@@ -1,7 +1,7 @@
 class BuellerController < ApplicationController
   def index
     @bueller = Student.all
-    if !@current_student
+    if current_student != @student
     redirect_to new_session_path
     end
   end
